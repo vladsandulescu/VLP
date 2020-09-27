a0945142c5941d3c418cbcac1966da52c3f7680d
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2020-09-27 12:05:49 +0200

    add paired attention model

---
+++ scripts/hm_pa_hpc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export PYTHONPATH=$PYTHONPATH:./
+
+python ./vlp/run_img2txt_dist.py \
+  --output_dir /work1/paupo/playground/hmm/lib/VLP/output_pa/hm \
+  --model_recover_path /work1/paupo/playground/hmm/lib/VLP/model/pretrained_model/cc_g8_lr1e-4_batch512_s0.75_b0.25/model.30.bin \
+  --image_root /work1/paupo/playground/hmm/data/img \
+  --src_file /work1/paupo/playground/hmm/data/ \
+  --region_det_file_prefix /work1/paupo/playground/hmm/data/imgfeat/region_feat_gvd_wo_bgd/feat_cls_1000/hm_detection_vg_100dets_vlp_checkpoint_trainval \
+  --region_bbox_file /work1/paupo/playground/hmm/data/imgfeat/region_feat_gvd_wo_bgd/raw_bbox/hm_detection_vg_100dets_vlp_checkpoint_trainval_bbox.h5 \
+  --dataset hm-paired \
+  --do_train \
+  --split train+dev \
+  --eval_split dev \
+  --train_batch_size 64 \
+  --eval_batch_size 64 \
+  --tasks hm-paired-attn \
+  --num_train_epochs 5 \
+  --learning_rate 2e-5 \
+  --warmup_proportion 0.025 \
+  --new_segment_ids \
+  --always_truncate_tail \
+  --amp \
+  --enable_butd \
+  --s2s_prob 0 \
+  --bi_prob 1 \
+  --mask_prob 0 \
+  --max_pred 1 \
+  --num_workers 4
--- vlp/loader_utils.py
+++ vlp/loader_utils.py
@@ -24,6 +24,30 @@ def batch_list_to_batch_tensors(batch):
     return batch_tensors
 
 
+def batch_list_to_batch_tensors_paired(batch):
+    batch_tensors = []
+    paired_batch = [x for t in batch for x in t[:-1]]
+    label = [v for t in batch for k, v in t[-1].items()]
+    for x in zip(*paired_batch):
+        if isinstance(x[0], torch.Tensor):
+            batch_tensors.append(torch.stack(x))
+        else:
+            batch_tensors.append(torch.tensor(x, dtype=torch.long))
+    batch_tensors.append(torch.tensor(label, dtype=torch.long))
+    return batch_tensors
+
+
+def batch_list_to_batch_tensors_paired_eval(batch):
+    batch_tensors = []
+    paired_batch = [x for t in batch for x in t]
+    for x in zip(*paired_batch):
+        if isinstance(x[0], torch.Tensor):
+            batch_tensors.append(torch.stack(x))
+        else:
+            batch_tensors.append(torch.tensor(x, dtype=torch.long))
+    return batch_tensors
+
+
 class Pipeline():
     """ Pre-process Pipeline Class : callable """
 
